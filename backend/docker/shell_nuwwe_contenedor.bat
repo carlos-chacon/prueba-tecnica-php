a55c4fd4f9b77bb75e6edc8fc53e16cdc14df1ff
carlos <carlos.chacon@gexnova.com.co>
2023-05-25 17:58:35 -0500

    fix composer

--- backend/docker/shell_nuwwe_contenedor.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-ECHO ingresando por shell al contenedor workspace.
-
-@REM cd D:/#Usuario/Documents/docker_proyect/laradock-nuwwe 
-cd C:/Users/Carlos A. Chacon/docker_proyect/laradock-nuwwe
-docker-compose exec workspace bash
