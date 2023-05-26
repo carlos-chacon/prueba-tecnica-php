1c20208bd0b2ea2362f41a0f010b4d65759ab8a0
carlos <ccarch81@gmail.com>
2023-05-26 11:58:22 -0500

    frontend menu e instalación de librería en javascript para ver el mapa

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FullComponent } from './layouts/full/full.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: FullComponent,
+    children: [
+      { path: '', redirectTo: '/home', pathMatch: 'full' },
+      { path: "home", loadChildren: () => import("./modules/home/home.module").then((m) => m.HomeModule) },
+      { path: "weather-log", loadChildren: () => import("./modules/weather-log/weather-log.module").then((m) => m.WeatherLogModule) },
+    ]
+  },
+  {
+    path: "**",
+    redirectTo: "/",
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ frontend/src/app/modules/weather-log/weather-log.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { WeatherLogRoutingModule } from './weather-log-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    WeatherLogRoutingModule
+  ]
+})
+export class WeatherLogModule { }
