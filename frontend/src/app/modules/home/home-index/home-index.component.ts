1a292a5b48ca615d307c5189738f27cffa23faed
carlos <ccarch81@gmail.com>
2023-05-27 12:10:55 -0500

    Pintando los datos del servicio en el frontend

--- frontend/src/app/modules/home/home-index/home-index.component.ts
+++ frontend/src/app/modules/home/home-index/home-index.component.ts
@@ -7,7 +7,7 @@ import {
   icon,
   latLng,
   tileLayer,
-  Control
+  Control,
 } from 'leaflet';
 import { ApiService } from 'src/app/shared/services/api.service';
 
@@ -33,15 +33,15 @@ export class HomeIndexComponent implements AfterViewInit {
 
   cities = [
     { name: 'Miami', latitude: 25.7824792, longitude: -80.3851769 },
-    { name: 'Orlando', latitude: 28.4811202, longitude: -81.6541434 },
-    { name: 'New York', latitude: 40.6976307, longitude: -74.1448305 },
+    // { name: 'Orlando', latitude: 28.4811202, longitude: -81.6541434 },
+    // { name: 'New York', latitude: 40.6976307, longitude: -74.1448305 },
   ];
+
+  customControl = new CustomInfoControl('');
 
   constructor(private apiService: ApiService) {}
 
-  ngAfterViewInit(): void {
-    console.log('ngAfterViewInit');
-  }
+  ngAfterViewInit(): void {}
 
   onMapReady(map: Map) {
     console.log('onMapReady');
@@ -57,7 +57,7 @@ export class HomeIndexComponent implements AfterViewInit {
       icon: icon(iconMarker),
     }).addTo(this.map);
 
-    const control = new Control();
+    this.customControl.addTo(this.map);
 
     this.cities.forEach((city) => {
       this.apiService
@@ -70,10 +70,14 @@ export class HomeIndexComponent implements AfterViewInit {
             const markerCity = new Marker(
               latLng(city.latitude, city.longitude),
               {
-                icon: icon(iconMarker)
+                icon: icon(iconMarker),
               }
             ).addTo(this.map);
-            markerCity.bindPopup(this.getHtmlPopup(), {keepInView: true}).openPopup();
+            markerCity.bindPopup(this.getHtmlPopup(resp)).openPopup();
+            this.customControl.update({
+              name: city.name,
+              humidity: resp.current.humidity,
+            });
           },
           error: (err) => {
             console.log(err);
@@ -85,30 +89,65 @@ export class HomeIndexComponent implements AfterViewInit {
   onMapClick(event: LeafletMouseEvent) {
     const lat = event.latlng.lat;
     const lng = event.latlng.lng;
-    this.marker.setLatLng(event.latlng);
-    this.marker
-      .bindPopup(this.getHtmlPopup())
-      .openPopup();
-    console.log('Latitud:', lat);
-    console.log('Longitud:', lng);
+    this.apiService
+      .getObs(`/v1/ciudades/obtener-humedad/${lat}/${lng}`)
+      .subscribe({
+        next: (resp: any) => {
+          console.log(resp);
+          this.marker.setLatLng(latLng(lat, lng));
+          this.marker.bindPopup(this.getHtmlPopup(resp)).openPopup();
+          // this.customControl.update({
+          //   name: '',
+          //   humidity: resp.current.humidity,
+          // });
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
+    // this.marker.setLatLng(event.latlng);
+    // this.marker
+    //   .bindPopup(this.getHtmlPopup())
+    //   .openPopup();
+    // console.log('Latitud:', lat);
+    // console.log('Longitud:', lng);
   }
 
-  getHtmlPopup() {
+  getHtmlPopup(props: any) {
     return `
     <ul class="list-none list-inside">
       <li class="mb-1">Ciudad: Bucaramanga</li>
-      <li class="mb-1">Humedad: 90%</li>
-      <li class="mb-1">Temperatura: 20 °C</li>
+      <li class="mb-1">Humedad: ${props.current.humidity}%</li>
+      <li class="mb-1">Temperatura: ${props.current.temp} °C</li>
       <li class="mb-1">
-      <div class="flex gap-x-4">
-        <img class="h-12 w-12 flex-none rounded-full" src="https://openweathermap.org/img/w/03n.png" alt="">
-        <div class="min-w-0 flex-auto">
-          <p class="">nubes dispersas</p>
+        <div class="flex gap-x-4">
+          <img class="h-12 w-12 flex-none rounded-full" src="https://openweathermap.org/img/w/${props.current.weather[0].icon}.png" alt="">
+          <div class="min-w-0 flex-auto inline-block">
+            <p class="">${props.current.weather[0].description}</p>
+          </div>
         </div>
-      </div>
       </li>
     </ul>
     `;
   }
 }
 
+export class CustomInfoControl extends Control {
+  controlDiv = document.createElement('div');
+  constructor(private infoText: string) {
+    super({ position: 'topright' }); // Puedes ajustar la posición del control aquí
+  }
+
+  override onAdd(map: Map): HTMLElement {
+    // const controlDiv = document.createElement('div');
+    this.controlDiv.innerHTML = '<h4>Humedad por ciudad:</h4>';
+    this.update();
+    return this.controlDiv;
+  }
+
+  update(props?: any) {
+    this.controlDiv.innerHTML += props
+      ? '<b>' + props.name + ': </b>' + props.humidity + ' % <br />'
+      : '';
+  }
+}
